ec48760d189ef9774deacb13b95e2093718ec15b
Sion Yap <sionpeterson@gmail.com>
2026-03-27 12:11:57 -0400

    in-class changes

--- src/tree/OrgChartImpl.java
+++ src/tree/OrgChartImpl.java
@@ -7,6 +7,55 @@ public class OrgChartImpl implements OrgChart{
 
 	//Employee is your generic 'E'..
 	private List<GenericTreeNode<Employee>> nodes = new ArrayList<>();
+
+	@Override
+	public void addRoot(Employee e) {
+		// TODO Auto-generated method stub
+		GenericTreeNode<Employee> rootEmployee = new GenericTreeNode<Employee>(e);
+		nodes.add(rootEmployee);
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addDirectReport(Employee manager, Employee newPerson) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < nodes.size(); i++)
+		{
+			GenericTreeNode<Employee> currentEmployee = nodes.get(i);
+			if(currentEmployee.data.equals(manager))
+			{
+				GenericTreeNode<Employee> newE = new GenericTreeNode<Employee>(newPerson);
+				
+				// adds child to current employee's list of children
+				nodes.get(i).addChild(newE);
+				nodes.add(newE);
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void removeEmployee(Employee firedPerson) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showOrgChartDepthFirst() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showOrgChartBreadthFirst() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
