8ae340b6d1acca1d625b519d03a14a9a724602cc
Sion Yap <sionpeterson@gmail.com>
2026-04-01 14:46:50 -0400

    added some changes to removeChild

--- src/tree/GenericTreeNode.java
+++ src/tree/GenericTreeNode.java
@@ -19,13 +19,29 @@ public class GenericTreeNode<E> {
 		// this one is a little harder.
 		// what do you do when the item has children?
 		// I suggest "give them to the parent"
-		if(theItem.children == null) // if item has no children
+		
+		// locate child in children ArrayList
+		for(int i = 0; i < children.size(); i++)
 		{
-			
-		}
-		else // if item has children, promote
-		{
-			
+			if(children.get(i).equals(theItem))
+			{
+				if(children.get(i).children == null) // if item has no children,
+				{
+					// remove them from parent's list of children -> done
+					children.remove(children.get(i));
+				}
+				else // if item has children, give them to its parent
+				{
+					// kind of a parent.addChild([its children])
+					// remove it from its parent's list of children -> done
+					for(int j = 0; j < children.get(i).children.size(); j++)
+					{
+						children.add(children.get(i).children.get(j));
+					}
+					children.remove(children.get(i));
+				}
+			break;
+			}
 		}
 	}
 	
